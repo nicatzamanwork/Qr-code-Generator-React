be9b76ac40ea375f2107f0cccb1f2cf0a4a1a726
nicatzamanwork <webdevelopernicat@gmail.com>
2023-02-15 15:32:09 +0400

    QR-code generator is ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import ReactDOM from "react-dom";
+import { QRCodeSVG } from "qrcode.react";
+import { QRCodeCanvas } from "qrcode.react";
+import { useState } from "react";
 function App() {
+  const [text, setText] = useState("");
+  const [qr, setQR] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setQR(text);
+    setText("");
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <QRCodeSVG value={qr} />
+
+      <form onSubmit={handleSubmit} className="input-here">
+        <p>Enter your text here</p>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+        />
+        <button>Generate</button>
+      </form>
     </div>
   );
 }
